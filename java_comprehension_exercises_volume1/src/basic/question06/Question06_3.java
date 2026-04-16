b00cf8a8d0df8986841797190f0fa6b51b624732
sanamiki <sanamiki@C010422801.zipangu.cac.co.jp>
2026-04-16 15:15:42 +0900

    Lesson6完了

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -31,8 +31,8 @@ public class Question06_3 {
 			buyFlag = Integer.parseInt(choicei);
 
 			System.out.println("購入した鉛筆の本数は" + amount + "本です。");
-
-			System.out.println("購入した鉛筆の合計金額は" + (amount + price) + "本です。");
+			
+			System.out.println("購入した鉛筆の合計金額は" +(amount+price)  + "本です。");
 
 		}
 
